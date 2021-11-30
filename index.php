c9113b455d4e672d46d4d4633e1ed12a33d7a041
Adem <adila78@hotmail.fr>
2021-11-30 12:01:27 +0100

    up

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+//echo file_put_contents("test.txt","je suis entrain d'ecrire . Test!");
+//$myfile = fopen("test.txt", "a") or die("Unable to open file!");
+//fwrite($myfile," mon deuxième texte");           // rajout du deuxième texte
+//echo "c'est fini ";
+//echo file_get_contents("test.txt"); affichage d'un html 
+var_dump(file("https://www.w3schools.com"));
+
+?>
